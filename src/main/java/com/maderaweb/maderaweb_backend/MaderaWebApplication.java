03ec27386a084030bc1d005ae082bfed70e36077
Beelzebud <eliholsan1509@gmail.com>
2025-10-27 19:54:37 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/maderaweb/maderaweb_backend/MaderaWebApplication.java
@@ -0,0 +1,13 @@
+package com.maderaweb.maderaweb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaderaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaderaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maderaweb/maderaweb_backend/MaderaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maderaweb.maderaweb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaderaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
